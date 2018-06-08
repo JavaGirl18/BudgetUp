083226195d65154988e6939b5b16b21d8cfd45d3
JavaGirl18 <valenciacooper89@gmail.com>
2018-06-08 11:03:24 -0400

    additional updates to show page

--- db/seeds.js
+++ db/seeds.js
@@ -7,7 +7,7 @@ const Budget = require('../models/budget')
 
 
 // Connect to Database
-mongoose.connect('mongodb://localhost/user')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('connected to mongoDB')
     })
@@ -45,6 +45,31 @@ User.remove()
             }
         })
 
+
+        const budget3 = new Budget({
+            expenses: {
+                name: "DayCare",
+                amount: 350,
+            },
+            income: {
+                type: "Yard work",
+                amount: 100,
+                //misc?
+            }
+        })
+
+
+        const budget4 = new Budget({
+            expenses: {
+                name: "Car insurance",
+                amount: 350,
+            },
+            income: {
+                type: "Child support",
+                amount: 700,
+                //misc?
+            }
+        })
         const comment1 = new Comment({
             body: "new comment"
         })
@@ -71,16 +96,43 @@ User.remove()
             comment: [comment2]
         })
 
+        const finGoals3 = new FinGoal({
+            name: "Save 1000 dollars",
+            goalAmount: 1000,
+            currentAmount: 200,
+            dueDate: new Date(2018, 10, 15),
+            budgetBar: 0,
+            comment: [comment2]
+        })
+
+        const finGoals4 = new FinGoal({
+            name: "pay off student loans",
+            goalAmount: 10000,
+            currentAmount: 1500,
+            dueDate: new Date(2020, 10, 15),
+            budgetBar: 0,
+            comment: [comment2]
+        })
+
         const user1 = new User({
-            name: "Oprah",
-            email: "oprahwin@own.com",
+            name: "Mike",
+            email: "mike@ga.com",
             password: "xxx",
-            pic: "https://pmcvariety.files.wordpress.com/2018/01/oprah-winfrey-backstage-golden-globes.jpg?w=1000&h=563&crop=1",
+            pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQmjcQafkuES7Wnflw3jHLbIZ8_D8yP0V5SfbV5svJgL6HJmL8",
             budget: [budget1],
             financialGoals: [finGoals1],
         })
 
         const user2 = new User({
+            name: "Paul",
+            email: "paul@ga.com",
+            password: "xxx",
+            pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHVl72cONzwQL7f8eFaepPl9Bo9IAVkBCQf0J_Yna1o2K4Ue8n5Q",
+            budget: [budget3],
+            financialGoals: [finGoals3],
+        })
+
+        const user3 = new User({
             name: "Serena",
             email: "serena@court.com",
             password: "xxx",
@@ -89,7 +141,16 @@ User.remove()
             financialGoals: [finGoals2]
         })
 
-        const users = [user1, user2]
+        const user4 = new User({
+            name: "Oprah",
+            email: "oprahwin@own.com",
+            password: "xxx",
+            pic: "https://pmcvariety.files.wordpress.com/2018/01/oprah-winfrey-backstage-golden-globes.jpg?w=1000&h=563&crop=1",
+            budget: [budget4],
+            financialGoals: [finGoals4],
+        })
+
+        const users = [user1, user2, user3, user4]
 
 
         // save test data
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require ('mongoose')
-mongoose.connect('mongodb://localhost/user')
+// mongoose.connect('mongodb://localhost/user')
 
 
 /* GET home page. */
